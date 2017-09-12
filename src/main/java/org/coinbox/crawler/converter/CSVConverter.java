13dbe464ec0be9123a4f2f2ca1038eaf57c1c891
Andrii Sushko <andrew_sushko@outlook.com>
2017-09-12 22:02:14 +0300

    [FIX] autoclosing CSVWriters via try-with-resources

--- src/main/java/org/coinbox/crawler/converter/CSVConverter.java
+++ src/main/java/org/coinbox/crawler/converter/CSVConverter.java
@@ -11,27 +11,18 @@ import org.coinbox.crawler.domain.Table;
 import com.opencsv.CSVWriter;
 
 public class CSVConverter {
-	
-	public void convertToCSV(List<Table> csvData){
-		
-		CSVWriter writer = null;
-		try {
-			for(Table currentTable : csvData){
-				System.out.println("Process " + currentTable.getName());
-				
-				writer = new CSVWriter(new FileWriter(Config.CSV_FOLDER + currentTable.getName() + ".csv"));
-				writer.writeAll(currentTable.getData());
-			}
-		} catch (IOException e) {
-			System.out.println("Convertor error " + e.getMessage());
-		} finally{
-			try {
-				writer.flush();
-				writer.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-	}
+
+    public void convertToCSV(List<Table> csvData) {
+
+
+        for (Table currentTable : csvData) {
+            System.out.println("Process " + currentTable.getName());
+
+            try (CSVWriter writer = new CSVWriter(new FileWriter(Config.CSV_FOLDER + currentTable.getName() + ".csv"))) {
+                writer.writeAll(currentTable.getData());
+            } catch (IOException e) {
+                System.out.println("Convertor error " + e.getMessage());
+            }
+        }
+    }
 }
