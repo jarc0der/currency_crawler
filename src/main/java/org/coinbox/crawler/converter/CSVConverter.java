365eac4a16c650f48b036e0ccfc0626f497684e8
= <tuzhanskyi>
2017-10-10 22:27:32 +0300

    parallelism with Future and ExecuteService

--- src/main/java/org/coinbox/crawler/converter/CSVConverter.java
+++ src/main/java/org/coinbox/crawler/converter/CSVConverter.java
@@ -6,12 +6,17 @@ import org.coinbox.crawler.domain.Table;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CSVConverter {
 
     public void convertToCSV(Stream<Table> csvData) {
-        csvData.forEach(currentTable ->{
+        csvData.parallel().forEach(currentTable ->{
             System.out.println("Process " + currentTable.getName());
             try (CSVWriter writer = new CSVWriter(new FileWriter(Config.CSV_FOLDER + currentTable.getName() + ".csv"))) {
                 writer.writeAll(currentTable.getData());
@@ -19,5 +24,30 @@ public class CSVConverter {
                 System.out.println("Convertor error " + e.getMessage());
             }
         });
+    }
+
+    public void parallelConvertToCSV(Stream<Table> csvData){
+        ExecutorService exec = null;
+
+        try {
+            exec = Executors.newCachedThreadPool();
+
+            List<Table> tableList = csvData.collect(Collectors.toList());
+
+            for (Table table : tableList) {
+                exec.submit(() -> new CSVConverter().convertSingleData(table));
+            }
+        }finally{
+            exec.shutdown();
+        }
+    }
+
+    private void convertSingleData(Table currentTable){
+        System.out.println("Process " + currentTable.getName());
+        try (CSVWriter writer = new CSVWriter(new FileWriter(Config.CSV_FOLDER + currentTable.getName() + ".csv"))) {
+            writer.writeAll(currentTable.getData());
+        } catch (IOException e) {
+            System.out.println("Convertor error " + e.getMessage());
+        }
     }
 }
--- src/main/java/org/coinbox/crawler/core/FileCrawler.java
+++ src/main/java/org/coinbox/crawler/core/FileCrawler.java
@@ -12,8 +12,13 @@ import org.coinbox.crawler.parser.Parser;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FileCrawler implements Crawler, FileHandler{
@@ -34,18 +39,29 @@ public class FileCrawler implements Crawler, FileHandler{
 		List<String> dataLinks = readFile(fileName);
 		
 		//for each link execute parser and get necesary content
-		Stream<Table> parsedData = processLinks(dataLinks);
+		Instant i1 = Instant.now();
+		Stream<Table> parsedData = parallelProcessLinks(dataLinks, 20);
+		Instant i2 = Instant.now();
+
+		System.out.println("Parrallel time: " + Duration.between(i1, i2));
 		
 		//System.out.println("Tables count: " + parsedData.size());
-		
+
+		System.out.println("Here 1");
 		//convert parsed content to CSV files
-		converter.convertToCSV(parsedData);
+		Instant i3 = Instant.now();
+		converter.parallelConvertToCSV(parsedData);
+		Instant i4 = Instant.now();
+
+		System.out.println("Here 2");
+		System.out.println("CSV time: " + Duration.between(i3, i4));
 		
 		//clear folder with csv
 		
 		//create ZIP from all csv files
+		System.out.println("Here 3");
 		File dir = new File(Config.RESOURCE_FOLDER);
-		
+
 		List<File> files = (List<File>)FileUtils.listFiles(dir, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
 		
 		compressor.compress(new ArrayList<>(files));
@@ -65,6 +81,47 @@ public class FileCrawler implements Crawler, FileHandler{
 	
 	private Stream<Table> processLinks(List<String> dataLinks){
 		return dataLinks.parallelStream().map(link -> (Table)parser.parserData(link));
+	}
+
+	private Stream<Table> parallelProcessLinks(List<String> dataLinks, int nThread){
+		//init executor
+		ExecutorService exec = null;
+		Stream<Table> stream = null;
+
+		try{
+			exec = Executors.newFixedThreadPool(nThread);
+			//create tasks
+			List<Future<Table>> futures = new ArrayList<>();
+
+			for(String link : dataLinks){
+				System.out.println("add task");
+				futures.add(exec.submit(() -> (Table)parser.parserData(link)));
+			}
+
+			//retrieve Data from Future objects
+			stream = futures.stream().map(FileCrawler::dataExtractor);
+
+		} finally {
+			exec.shutdown();
+		}
+		//return Stream<Table>
+
+		return stream;
+	}
+
+	private static Table dataExtractor(Future<Table> tableFuture){
+		Table table = null;
+		try {
+			table = tableFuture.get();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
+		//bug, some times we get null tables
+		if(table == null)
+			table = new Table();
+
+		return table;
 	}
 
 }
